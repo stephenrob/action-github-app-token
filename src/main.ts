cb9787ac1dc43dd55037492e2c061008bd661651
Stephen Robinson <stephenrob@users.noreply.github.com>
2021-04-08 14:26:08 +0000

    Add required input for base url

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import {createAppAuth} from '@octokit/auth-app';
 import {Octokit} from '@octokit/rest';
 import * as core from '@actions/core';
+import { request as r1 } from 'http';
+import { request as r2 } from '@octokit/request';
 
 async function run(): Promise<void> {
   try {
     const privateKey: string = core.getInput('private_key');
     const id: string = core.getInput('app_id');
+    const baseUrl: string = core.getInput('base_url');
     const appOctokit = new Octokit({
       authStrategy: createAppAuth,
       auth: {
-        id,
-        privateKey,
+        appId: id,
+        privateKey: privateKey,
+        request: r2.defaults({baseUrl: baseUrl})
       },
+      baseUrl: baseUrl,
     });
 
     const {data} = await appOctokit.apps.listInstallations();
